e0d20800be85dc088461b464765a4a6508fe2e21
Maxim Martynov <max.mrtnv@gmail.com>
2023-06-11 14:37:39 +0400

    Fix: Scheduled task

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PriceParserService } from './price-parser/price-parser.service';
 import { AdminController } from './admin.controller';
 import { DataModule } from 'src/data/data.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports: [DataModule],
+  imports: [DataModule, ScheduleModule],
   providers: [PriceParserService],
   controllers: [AdminController],
 })
