8abfaecfaf9dc6c86fcd3d03bb5d7ab8ee502a6a
Maxim Martynov <max.mrtnv@gmail.com>
2023-06-23 11:45:48 +0400

    Implementation: Updating consumables list by XLSX

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -10,6 +10,7 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import { PriceParserService } from './price-parser/price-parser.service';
 import { ApiKeyGuard } from 'src/core/guards/api-key.guard';
 import * as XLSX from 'xlsx';
+import { DB } from 'src/data/airtable-db';
 
 @Controller('admin')
 export class AdminController {
@@ -34,30 +35,97 @@ export class AdminController {
   @SetMetadata('apiKey', '34DXKqh^XDofLnN^Gk9ZaySoXS@xrJ3JEqT')
   @UseInterceptors(FileInterceptor('file'))
   async playground(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      return {
+        error: 'No file provided',
+      };
+    }
+  }
+
+  async updateConsumablesListByXlsx(@UploadedFile() file: Express.Multer.File) {
     const workbook = XLSX.read(file.buffer, { type: 'buffer' });
     const worksheetName = workbook.SheetNames[0];
     const worksheet = workbook.Sheets[worksheetName];
+    const rows = XLSX.utils.sheet_to_json(worksheet, {});
 
-    if (worksheet['!merges']) {
-      worksheet['!merges'] = worksheet['!merges'].filter((range) => {
-        // Check if the range corresponds to 'A1:C1'
-        const isUnmergedRange =
-          range.s.c === 0 &&
-          range.e.c === 2 && // Columns A-C (0-indexed)
-          range.s.r === 0 &&
-          range.e.r === 0; // Row 1 (0-indexed)
+    const pricesAnsStocks = await DB()
+      .PricesAndStockTable.select({
+        view: 'База данных',
+      })
+      .all();
 
-        return !isUnmergedRange;
-      });
+    const pricesAnsStocksMap = pricesAnsStocks.reduce((acc, item) => {
+      const code = (item.get('Код товара') as string)?.trim()?.toUpperCase();
+      if (!code) return acc;
+
+      acc[code] = item;
+      return acc;
+    }, {});
+
+    const consumables = rows
+      .filter((row) => row['Код'] && row['Наименование'])
+      .map((row) => ({
+        code: row['Код'].trim().toUpperCase(),
+        name: row['Наименование'].trim(),
+      }));
+
+    const existingConsumables = await DB()
+      .ConsumablesTable.select({
+        view: 'Grid view',
+      })
+      .all();
+
+    const exisitingConsumablesMap = existingConsumables.reduce((acc, item) => {
+      const code = (item.get('Артикул') as string)?.trim()?.toUpperCase();
+      if (!code) return acc;
+
+      acc[code] = item;
+      return acc;
+    }, {});
+
+    const newConsumables = consumables.filter((consumable) => {
+      return !exisitingConsumablesMap[consumable.code];
+    });
+
+    const newConsumablesRecords = newConsumables.map((item) => {
+      const priceStockItem = pricesAnsStocksMap[item.code];
+      const priceStockItemId = priceStockItem?.getId();
+
+      const newRecord = {
+        fields: {
+          Артикул: item.code,
+          Наименование: item.name,
+          'Цены и остатки': priceStockItemId ? [priceStockItemId] : [],
+        },
+      };
+
+      if (newRecord.fields['Цены и остатки'].length === 0) {
+        delete newRecord.fields['Цены и остатки'];
+      }
+
+      return newRecord;
+    });
+
+    const GROUP_SIZE = 10;
+    const newConsumablesGroups = newConsumablesRecords.reduce(
+      (acc, item) => {
+        const lastGroup = acc[acc.length - 1];
+
+        if (lastGroup.length < GROUP_SIZE) {
+          lastGroup.push(item);
+        } else {
+          acc.push([item]);
+        }
+
+        return acc;
+      },
+      [[] as any[]],
+    );
+
+    for (const group of newConsumablesGroups) {
+      await DB().ConsumablesTable.create(group);
     }
 
-    delete worksheet['A1'];
-    worksheet['C1'] = {
-      v: 'Артикул',
-      t: 's',
-    };
-
-    const rows = XLSX.utils.sheet_to_json(worksheet, {});
-    console.log(rows);
+    console.log(newConsumables);
   }
 }
